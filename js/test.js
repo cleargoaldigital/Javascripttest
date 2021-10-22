79b04a461472e8d9d05cfc9e2fc6672485c8c77c
cleargoaldigital <cleargoal.ng@gmail.com>
2021-10-22 23:29:13 +0100

    js

--- js/test.js
+++ js/test.js
@@ -10,3 +10,28 @@ console.log(fifthCity);
 
 let updatedCity = bigCities.push('Gbogan', 'Ilaro');
 console.log(updatedCity);
+
+// Creating a price list
+
+const list = document.querySelector('.price');
+
+const totalBox = document.querySelector('.new');
+
+let total = 0;
+list.innerHTML = '';
+totalBox.textContent = '';
+
+let products = ['underpants: 6.99', 'socks: 5.99','T-shirt: 14.99', 'Trousers: 31.99', 'Shoes: 23.99'];
+
+for (let i = 0; i < products.length; i++) {
+    let productRange = products[i].split(':');
+    let productName = productRange[0];
+    let productPrice = Number(productRange[1]);
+    total += price;
+    let itemText =  productName + ' - $' + productPrice;
+    let listItem = document.createElement('h3');
+    listItem.textContent = itemText;
+    list.appendChild(listItem);
+}
+
+totalBox.textContent = 'Total: $' + 'Total: $' + total.toFixed(2);

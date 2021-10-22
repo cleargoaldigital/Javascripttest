06fbe117f8aacc7b105da79125b5fa43974a58bf
cleargoaldigital <cleargoal.ng@gmail.com>
2021-10-22 19:49:27 +0100

    Javascript
    A series of lessons in Javascript.

---
+++ js/test.js
@@ -0,0 +1,12 @@
+let bigCities = ['Ibadan', 'Lagos', 'Abeokuta', 'Osogbo', 'Ife', 'Ilesha', 'Oyo', 'Saki', 'Ogbomosho', 'Ilorin', 'Kuta', 'Iseyin', 'Ijebu-Ode'];
+console.log(bigCities);
+
+let cityNumber = bigCities.length;
+
+cityNumber;
+
+let fifthCity = bigCities[4];
+console.log(fifthCity);
+
+let updatedCity = bigCities.push('Gbogan', 'Ilaro');
+console.log(updatedCity);
